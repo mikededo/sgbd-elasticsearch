b0604d00cb2b684e4ad6bac97fd1e853f7deb199
AdriaRibas <u1959540@campus.udg.edu>
2021-11-22 21:43:01 +0100

    Started implementation of GET players REST calls.

--- packages/server/index.js
+++ packages/server/index.js
@@ -19,10 +19,49 @@ app.post('/login', (req, res) => {
 app.post('/register', (req, res) => {
     const body = req.body;
     const name = body.name;
-    const lastName = body.last_name;
+    const lastName = body.lastName;
     const email = body.email;
     const password = body.password;
     res.send(
         `Name: ${name}. Last name: ${lastName}. Email: ${email}. Password: ${password}`
     );
+});
+
+app.get('/players', async (req, res) => {
+    const result = await ElasticClient.search({
+        index: 'players',
+        body: req.body
+    });
+
+    res.send(result.body.hits.hits.map(x => x._source));
+});
+
+app.get('/user/:name/fav-players', (req, res) => {
+    const players = [1, 5, 1205, 4589, 9999, 12125, 364, 7106];
+
+    const result = players.map(async x => {
+        const result = await ElasticClient.search({
+            index: 'players',
+            body: { query: { match: { id: x } } }
+        });
+        return result.body.hits.hits[0]._source;
+    });
+
+    res.send(result);
+});
+
+app.post('/user/:name/fav-players', (req, res) => {
+});
+
+app.delete('/user/:name/fav-players', (req, res) => {
+
+});
+
+app.get('/players/:id', async (req, res) => {
+    const result = await ElasticClient.search({
+        index: 'players',
+        body: { query: { match: { id: req.params.id } } }
+    });
+
+    res.send(result.body.hits.hits[0]._source);
 });
